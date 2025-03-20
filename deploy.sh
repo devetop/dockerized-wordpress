0f94f93950a4db1de41c11fdca819c93264b0452
erfan dwi prasetya <erfan.dwi.prasetya@tonjoo.com>
2025-03-20 10:58:07 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ cd /home/tonjoo/repo/wordpress.git
 git fetch origin master:master
 git --work-tree=/home/tonjoo/dockerized-wordpress --git-dir=/home/tonjoo/repo/wordpress.git checkout master -f
 chmod +x /home/tonjoo/dockerized-wordpress/wrapper.sh
-bash /home/tonjoo/dockerized-wordpress/wrapper.sh dev-local dev-tongkolspace dev-proxy down
-bash /home/tonjoo/dockerized-wordpress/wrapper.sh dev-local dev-tongkolspace dev-proxy up -d 
+bash /home/tonjoo/dockerized-wordpress/wrapper.sh dev-local dev-tongkolspace down
+bash /home/tonjoo/dockerized-wordpress/wrapper.sh dev-local dev-tongkolspace up -d 
